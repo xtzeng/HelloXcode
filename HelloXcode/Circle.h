a6cca1d9b25bc2959e7f79ce705f3a24b6cf4011
曾小堤 <cengxiaodi@cengxiaodideMacBook-Air.local>
2015-08-17 21:11:12 +0800

    first commit

---
+++ HelloXcode/Circle.h
@@ -0,0 +1,13 @@
+//
+//  Circle.h
+//  HelloXcode
+//
+//  Created by 曾小堤 on 15/8/14.
+//  Copyright (c) 2015年 曾小堤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Circle : NSObject
+
+@end
---
+++ HelloXcode/Circle.m
@@ -0,0 +1,13 @@
+//
+//  Circle.m
+//  HelloXcode
+//
+//  Created by 曾小堤 on 15/8/14.
+//  Copyright (c) 2015年 曾小堤. All rights reserved.
+//
+
+#import "Circle.h"
+
+@implementation Circle
+
+@end
